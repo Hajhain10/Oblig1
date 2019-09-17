c75e8b79eeca178ba73970bf97811d630034bd0d
Hajhain Nirmalan <hajankaka12@hotmail.com>
2019-09-17 11:33:49 +0200

    Fjernet æ,ø,å fikk vite at arraylist ikke skal brukes

--- no/no/oslomet/cs/algdat/Oblig1/Oblig1.java
+++ no/no/oslomet/cs/algdat/Oblig1/Oblig1.java
@@ -17,12 +17,12 @@ public class Oblig1 {
             //stoppeverdien er fra < til
             int minste = fra;
             for (int i = fra; i < til; i++) {
-                //finner indeksen til den minste for å sette den til fra plassen
+                //finner indeksen til den minste for a sette den til fra plassen
                 if (a[minste] > a[i]) {
                     minste = i;
                 }
             }
-            //bytter fra til å bli den minste verdien
+            //bytter fra til a bli den minste verdien
             int temp = a[fra];
             a[fra] = a[minste];
             a[minste] = temp;
@@ -48,7 +48,7 @@ public class Oblig1 {
         while (v <= h) {
 
             while (array[v] < pivot) {
-                // finner indeksen som er høyere enn midtverdi
+                // finner indeksen som er hoyere enn midtverdi
                 v++;
             }
 
@@ -116,6 +116,17 @@ public class Oblig1 {
         }
 
     }
+    //samme byttmetode som i hjelpemetoder i oblig1test
+    //denne tar inn char istedenfor int
+    private static void bytt(char[] a, int v, int h) {
+        while (v < h) {
+            char temp = a[v];
+            a[v] = a[h];
+            a[h] = temp;
+            h--;
+            v++;
+        }
+    }
 
 
     private Oblig1() {
@@ -127,9 +138,9 @@ public class Oblig1 {
         if(a.length <= 0){
             throw new NoSuchElementException("Arrayet kan ikke inneholde 0 tall");
         }
-        //looper igjennom for å endre plass
+        //looper igjennom for a endre plass
         for(int i = 1; i < a.length; i++){
-            //bobler opp den største verdien
+            //bobler opp den storste verdien
             if(a[i-1]>a[i]){
                 int temp = a[i];
                 a[i]=a[i-1];
@@ -137,24 +148,24 @@ public class Oblig1 {
             }
 
         }
-        //etter at vi har bobla vil den største verdien ligge i det siste elementet i arrayen
+        //etter at vi har bobla vil den storste verdien ligge i det siste elementet i arrayen
         return a[a.length-1];
 
     }
 
     public static int ombyttinger(int[] a) {
         //throw new NotImplementedException();
-        //dersom vi ikke får inn noe kastes en exception
+        //dersom vi ikke far inn noe kastes en exception
         if(a.length <= 0){
             throw new NoSuchElementException("Arrayet kan ikke inneholde 0 tall");
         }
 
         //denne teller antall ombyttinger
         int antall_byttinger = 0;
-        //looper igjennom for å telle inversjoner
+        //looper igjennom for a telle inversjoner
         for(int i = 1; i < a.length; i++){
             //bruker samme konsept som i oppgaven over.
-            //vi øker antall ombyttinger hver gang vi kommer inn i if setningen
+            //vi oker antall ombyttinger hver gang vi kommer inn i if setningen
             if(a[i-1]>a[i]){
                 antall_byttinger++;
                 int temp = a[i];
@@ -163,7 +174,7 @@ public class Oblig1 {
             }
 
         }
-        //returnerer antall iversjoner. vi ser på bytting av et par = 1 ombytting
+        //returnerer antall iversjoner. vi ser pa bytting av et par = 1 ombytting
         return antall_byttinger;
 
     }
@@ -174,19 +185,19 @@ public class Oblig1 {
         if(a.length==0){
             return 0;
         }
-        //Starter på 1 siden d første tallet alltid vil være unikt
+        //Starter pa 1 siden d forste tallet alltid vil vaere unikt
         int antall = 1;
-        //går igjennom arrayet som vi har
+        //gar igjennom arrayet som vi har
         for(int i = 1; i< a.length; i++){
-            //vi trenger kun å sjekke med tallet foran for å se om alt er sortert
+            //vi trenger kun a sjekke med tallet foran for a se om alt er sortert
             int temp = a[i-1];
             int temp1 = a[i];
             //dersom det ikke er sortert kastes en exception
             if(temp>temp1){
                 throw new IllegalStateException("Input arrayet er ikke sortert");
             }
-            //dersom a[i] og a[i-1] er forskjellig vet vi at vi har fått inn et nytt unikt tall.
-            //derfor øker vi antall
+            //dersom a[i] og a[i-1] er forskjellig vet vi at vi har fatt inn et nytt unikt tall.
+            //derfor oker vi antall
             else if(temp!=temp1){
                 antall++;
             }
@@ -199,7 +210,7 @@ public class Oblig1 {
     ///// Oppgave 3 //////////////////////////////////////
     public static int antallUlikeUsortert(int[] a) {
         //throw new NotImplementedException();
-        //hjelpevariablen for å telle antall ulike
+        //hjelpevariablen for a telle antall ulike
         int unike = 0;
 
 
@@ -227,14 +238,14 @@ public class Oblig1 {
         }
         int partall = 0;
         int oddetall = 0;
-        //løkke for å sjekke om d kun r enten partall eller oddetall
+        //lokke for a sjekke om d kun r enten partall eller oddetall
         for(int i = 0; i<a.length; i++){
             if(a[i]%2==0){
                 partall++;
             }else
                 oddetall++;
         }
-        //hvis det kun er en av delene så sorterer vi hele llsta
+        //hvis det kun er en av delene sa sorterer vi hele llsta
         if(oddetall == 0 || partall == 0){
             quickSort(a,0,a.length-1);
             return;
@@ -242,20 +253,20 @@ public class Oblig1 {
 
         int left = 0;
         int right = a.length-1;
-        //får oddetall på venstre side og partall på høyre side.
-        // vi kan så sortere dem hver for seg
+        //far oddetall pa venstre side og partall pa hoyre side.
+        // vi kan sa sortere dem hver for seg
         while(left < right) {
             //dersom %2=0 vet vi at det er partall
             while (a[right] % 2 == 0) {
                 right--;
 
             }
-            //dersom %2!=0 så er det et oddetall
+            //dersom %2!=0 sa er det et oddetall
             while (a[left] % 2 != 0) {
                 left++;
 
             }
-            //bytter om verdiene for å få oddetall til venstre og partall til høyre
+            //bytter om verdiene for a fa oddetall til venstre og partall til hoyre
             if(left <= right){
                 int temp = a[left];
                 a[left] = a[right];
@@ -263,8 +274,8 @@ public class Oblig1 {
             }
 
         }
-        //bruker kvikksortering for å sortere partall og oddetall seperat.
-        //kvikksort koden ligger øverst i dette dokumentet
+        //bruker kvikksortering for a sortere partall og oddetall seperat.
+        //kvikksort koden ligger overst i dette dokumentet
         quickSort(a,0,right);
         quickSort(a,left,a.length-1);
 
@@ -275,12 +286,12 @@ public class Oblig1 {
 
         //throw new NotImplementedException();
 
-        //hvis vi ikke får inn noe så skal løkken avsluttes
+        //hvis vi ikke far inn noe sa skal lokken avsluttes
         if(a.length == 0){
             return;
         }else {
-            //vi tar vare på det siste tallet ettersom at dette blir glemt i
-            //i = a.length -1. denne verdien skal være i a[0] uansett situasjon
+            //vi tar vare pa det siste tallet ettersom at dette blir glemt i
+            //i = a.length -1. denne verdien skal vaere i a[0] uansett situasjon
             // derfor setter vi det til slutt.
             char siste = a[a.length - 1];
             for (int i = a.length - 1; i > 0; i--) {
@@ -293,7 +304,16 @@ public class Oblig1 {
 
     ///// Oppgave 6 //////////////////////////////////////
     public static void rotasjon(char[] a, int k) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        //kan ikke operere med en tom mengde
+        if(a.length == 0){
+            return;
+        }
+        //dersom k er storre enn lenden til a vil den reduseres
+        int rotasjoner = k%a.length;
+        if(rotasjoner<0){
+            rotasjoner = k+ a.length;
+        }
 
     }
 
@@ -302,26 +322,26 @@ public class Oblig1 {
     public static String flett(String s, String t) {
         //throw new NotImplementedException();
 
-        //splitter strengene vi får med "";
+        //splitter strengene vi faar med "";
         //ABC blir til A,B,C
         String[] String_s = s.split("");
         String[] String_t = t.split("");
 
-        //sluttordet sin lengde må være begge strengene sin lengde til sammen
+        //sluttordet sin lengde ma vaere begge strengene sin lengde til sammen
         String[] flettet = new String[String_s.length + String_t.length];
 
         // hjelpevaribler
-        // k brukes til å fylle flettet
-        // j og i skal gå igjennom s og t
+        // k brukes til a fylle flettet
+        // j og i skal ga igjennom s og t
         int i =0; int j = 0; int k = 0;
 
-        //denne gjøres til en av charene går tom for bokstaver
-        // tar først en bokstav fra s så en fra t....
+        //denne gjores til en av charene gar tom for bokstaver
+        // tar forst en bokstav fra s sa en fra t....
         while( i < String_s.length && j < String_t.length){
             flettet[k++] = String_s[i++];
             flettet[k++] = String_t[j++];
         }
-        //kun en av disse while løkkene vil kjøre
+        //kun en av disse while lokkene vil kjore
         // den skal kunne legge til resten av bokstavene som er igjen fra enten s eller t
         while(i < String_s.length){
             flettet[k++] = String_s[i++];
@@ -347,7 +367,7 @@ public class Oblig1 {
         if(s.length==0){
             return "";
         }
-        // for å finne lengden til sluttordet må vi legge sammen alle
+        // for a finne lengden til sluttordet ma vi legge sammen alle
         //lengdene til s
         int lengde = 0;
         for (int i = 0; i < s.length; i++) {
@@ -359,10 +379,10 @@ public class Oblig1 {
         //i er posisjonen den skal hente bokstaven fra
         for(int i = 0; i<svar.length; i++){
             //j velger hvilket ord bokstaven skal hentes fra
-            // feks s[1] vil være det andre ordet som vi fikk inn som parameter
+            // feks s[1] vil vaere det andre ordet som vi fikk inn som parameter
             for (int j = 0; j < s.length; j++) {
-                //dersom s[j],length() > i slår inn vet vi at ordet har en bokstav i charAt(i)
-                //og kan hente den. hvis den ikke slår inn vil det si at ordet har gått tom for bokstaver
+                //dersom s[j],length() > i slar inn vet vi at ordet har en bokstav i charAt(i)
+                //og kan hente den. hvis den ikke slar inn vil det si at ordet har gatt tom for bokstaver
                 if(s[j].length()>i){
                     svar[k]=s[j].charAt(i);
                     k++;
@@ -385,7 +405,7 @@ public class Oblig1 {
         if (a.length == 0) {
             return a;
         }
-        //endre array til arraylist ettersom at dette er enklere å jobbe med
+        //endre array til arraylist ettersom at dette er enklere a jobbe med
         ArrayList<Integer> liste = new ArrayList<>();
         for (int i = 0; i < a.length; i++) {
             liste.add(a[i]);
@@ -396,7 +416,7 @@ public class Oblig1 {
         //selection sort
         sorter(b,0,b.length);
 
-        //lager en ny array som skal være indekstabellen
+        //lager en ny array som skal vaere indekstabellen
         int[] kopi = new int[a.length];
         kopi[0] = liste.indexOf(b[0]);
         //dersom vi har duplikate tall vi vi endre verdiene til tallet som er brukt
@@ -404,8 +424,8 @@ public class Oblig1 {
 
         for (int i = 1; i < kopi.length; i++) {
             //dersom vi finner d samme tallet endrer vi tallet som er brukt
-            //slik at vi kan få tallet som ikke er brukt. feks om vi har 5,1,0,5 vil vi få
-            // indeks 0 i første omgang og indeks 4 i andre
+            //slik at vi kan fa tallet som ikke er brukt. feks om vi har 5,1,0,5 vil vi fa
+            // indeks 0 i forste omgang og indeks 4 i andre
             if (b[i] == b[i - 1]) {
                 liste.set(liste.indexOf(b[i]), storsteTall);
                 kopi[i] = liste.indexOf(b[i]);
@@ -424,9 +444,9 @@ public class Oblig1 {
         //sjekker at vi har tre tall minst
         int n = a.length;
         if (n < 3) {
-            throw new NoSuchElementException("Må ha minst tre tall");
+            throw new NoSuchElementException("Ma ha minst tre tall");
         }
-        //sorterer kun de tre første tallene ved hjelp av oppgaven over
+        //sorterer kun de tre forste tallene ved hjelp av oppgaven over
         int[] sorter = {a[0],a[1],a[2]};
         int[] sortert = indekssortering(sorter);
 
@@ -440,7 +460,7 @@ public class Oblig1 {
         int tredjminstverdi = a[tm];
 
         for (int i = 3; i < n; i++) {
-            //dersom tallet er mindre enn tredj minste tallet vil denne være en av tre minste tallene
+            //dersom tallet er mindre enn tredj minste tallet vil denne vaere en av tre minste tallene
             if (a[i] < tredjminstverdi) {
                 //dersom a[i] er minsre enn minsteverdi vil dette bli det minste tallet
                 if (a[i] < minstverdi) {
@@ -453,7 +473,7 @@ public class Oblig1 {
                     m = i;
                     minstverdi = a[m];
 
-                    //dersom den er større enn minste verdi vil den bli nest minste verdi
+                    //dersom den er storre enn minste verdi vil den bli nest minste verdi
                     //og nest minste verdi bli til den tredj minste.
                 } else if (a[i] < nestminstsverdi) {
                     tm = nm;
@@ -480,11 +500,11 @@ public class Oblig1 {
 
     public static boolean inneholdt(String a, String b) {
        // throw new NotImplementedException();
-        // dersom a får inn en tom streng så vil a være i b.
+        // dersom a far inn en tom streng sa vil a vaere i b.
         if(a.equals("")){
             return true;
         }
-        // en mengde som ikke er tom(a) vil ikke være i en tom mengde(b).
+        // en mengde som ikke er tom(a) vil ikke vaere i en tom mengde(b).
         if(!a.equals("") && b.equals("")){
             return false;
         }
@@ -498,13 +518,13 @@ public class Oblig1 {
 
 
         for (int i = 0; i < char2.length; i++){
-            //dersom char[j]==char[i] vil den ene bokstaven være lik
+            //dersom char[j]==char[i] vil den ene bokstaven vaere lik
             // vi fjerner denne bokstaven fra b slik at den ikke blit talt opp igjen/ flere ganger
-            // i tillegg til dette går vi til neste bokstav i streng a med hjelpervariabelen j++
+            // i tillegg til dette gar vi til neste bokstav i streng a med hjelpervariabelen j++
             if(char1[j]==char2[i]){
                 j++;
                 //dersom j == a.length() vil d si at alle bokstavene er funnnet i b
-                //som medfører at a er inneholdt i b
+                //som medforer at a er inneholdt i b
                 if(a.length() == j){
                     return true;
                 }
